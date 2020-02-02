7face9073905ea7949fa3341c68899c1aa1fefe8
ChristianDerricks <58296849+ChristianDerricks@users.noreply.github.com>
2020-02-02 13:40:12 +0100

    Create colorlist.R

---
+++ pkg/colorlist.R
@@ -0,0 +1,11 @@
+# This is a list of eight colors that are distinguishable for people with 
+# every type of color vision deficiency (often misleadingly called colorblindness).
+# see https://jfly.iam.u-tokyo.ac.jp/color/ for more information.
+cb_darkgrey      = rgb( 50,  50,  50, max=255)
+cb_blue          = rgb(  0, 114, 178, max=255)
+cb_orange        = rgb(230, 159,   0, max=255)
+cb_bluishgreen   = rgb(  0, 158, 115, max=255)
+cb_vermillion    = rgb(213,  94,   0, max=255)
+cb_skyblue       = rgb( 86, 180, 233, max=255)
+cb_reddishpurple = rgb(204, 121, 167, max=255)
+cb_yellow        = rgb(240, 228,  66, max=255)
