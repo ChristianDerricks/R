2113c352350dc8932c05aec18661488eb7f4710d
ChristianDerricks <58296849+ChristianDerricks@users.noreply.github.com>
2020-02-02 13:43:36 +0100

    Update ParetoDiagram.R

--- ParetoDiagram/ParetoDiagram.R
+++ ParetoDiagram/ParetoDiagram.R
@@ -1,37 +1,12 @@
-if(!is.null(dev.list())) dev.off()
-graphics.off()
+source('pkg/initalize.R', local = TRUE)
 
-# delete every variable
-remove(list = ls())
-
-library(tikzDevice)
-
-# This is a list of eight colors that are distinguishable for people with 
-# every type of color vision deficiency (often misleadingly called colorblindness).
-# see https://jfly.iam.u-tokyo.ac.jp/color/ for more information.
-cb_darkgrey      = rgb( 50,  50,  50, max=255)
-cb_blue          = rgb(  0, 114, 178, max=255)
-cb_orange        = rgb(230, 159,   0, max=255)
-cb_bluishgreen   = rgb(  0, 158, 115, max=255)
-cb_vermillion    = rgb(213,  94,   0, max=255)
-cb_skyblue       = rgb( 86, 180, 233, max=255)
-cb_reddishpurple = rgb(204, 121, 167, max=255)
-cb_yellow        = rgb(240, 228,  66, max=255)
-
-# manually paste the following two lines in the header of the tex file
-# %!TEX TS-program = lualatex
-# %!TeX encoding = utf8
-#tikz_path = '/R/ParetoDiagramm/tikz/'
-dir.create(file.path(tikz_path), showWarnings = FALSE)
-
-tikz_filename = 'ParetoDiagram.tex'
-tizeTEX_path_and_filename = paste(tikz_path, tikz_filename)
-#tikz(tizeTEX_path_and_filename, standAlone = TRUE, sanitize = TRUE)
+make_tikz = TRUE
+if (make_tikz) tikzRexport('ParetoDiagram.tex')
 
 # c(bottom, left, top, right) to set margins
-par(mar=c(7,6,2,6)) 
+par(mar=c(7,6,2,6))
 
-data <- read.csv('/R/ParetoDiagramm/data/pareto_data.csv', header = TRUE)
+data <- read.csv('/home/christian/Dokumente/Programme/R/ParetoDiagramm/data/pareto_data.csv', header = TRUE)
 yprop = data$defect[order(decreasing = TRUE, data$defect)]
 csumvec = 100*cumsum(yprop)/sum(yprop)
 
@@ -58,4 +33,4 @@ axis(4, at=bp+0.2, labels = paste(format(round(100*yprop/sum(yprop), 1), nsmall 
 axis(4, at=bp+0.2, labels = paste(format(round(csumvec, 1), nsmall = 1),'%'),              side=1, col = NA, line=2.85, hadj = 1)
 box()
 
-#dev.off()
+if (make_tikz) dev.off()
