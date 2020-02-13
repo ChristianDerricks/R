54f38c53a8a3e94900511a68f9a24b436a79fff3
ChristianDerricks <58296849+ChristianDerricks@users.noreply.github.com>
2020-02-13 17:39:20 +0100

    Update initalize.R

--- pkg/initalize.R
+++ pkg/initalize.R
@@ -1,18 +1,22 @@
-# prevent error messages displayed if there are no plots to clear
+# This file is used to initalize a few custom packages and do some basic stuff
+# so that it must not be included in every program making maintance easier.
+
+# Delete all variables from the global enviroment, remove all plots and
+# prevent error messages displayed if there are no plots to clear.
+remove(list = ls())
 if(!is.null(dev.list())) dev.off()
 graphics.off()
 
-# delete every variable
-remove(list = ls())
-
-# load colorlist with eight colors
+# Load a custom colorlist with eight colors.
 source('pkg/colorlist.R', local = TRUE)
 
-# load tikz handler
-source('pkg/tikz_export.R', local = TRUE)
+# A small package to check if RStudio is used
+source('pkg/isRStudio.R', local = TRUE)
 
-# tikz setup
-#make_tikz = TRUE
-#if (make_tikz) tikzRexport('ParetoDiagram.tex')
-# your code here
-#if (make_tikz) dev.off()
+# Check if RStudio is used, otherwise dont't load tikz_export.
+if (is.RStudio()) {
+    # load tikz handler
+    source('pkg/tikz_export.R', local = TRUE)
+  } else {
+    message('tikz_export works only with RStudio due to path and basename requierments')
+  }
